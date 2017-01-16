9689ec21bd84747630bab05608cb600dd4950e4d
zheng86 <80227320@qq.com>
2017-01-16 16:46:05 +0800

    crypto

--- mysite/view.py
+++ mysite/view.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 #coding=utf-8
 from django.shortcuts import render
+# from django.views.decorators.clickjacking import xframe_options_deny
+# from django.views.decorators.clickjacking import xframe_options_sameorigin
+from django.views.decorators.clickjacking import xframe_options_exempt
 
+@xframe_options_exempt
 def home(request):
     context          = {}
     context['hello'] = 'Hello World!'
     return render(request, 'home.html', context)
 
+@xframe_options_exempt
 def crypto(request):
     return render(request, 'crypto.html', {})
